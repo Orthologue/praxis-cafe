eedc90264f285e4edef170ba720a86834eb2dac5
alec aivazis <alec@aivazis.com>
2017-05-28 13:24:08 -0700

    initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+// external imports
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+// local imports
+import { AppBar } from './components'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <AppBar />
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+});
---
+++ components/layout/AppBar.js
@@ -0,0 +1,15 @@
+// external imports
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+const AppBar = () => (
+    <View style={styles.container}>
+
+    </View>
+)
+
+const styles = StyleSheet.create({
+
+})
+
+export default AppBar
