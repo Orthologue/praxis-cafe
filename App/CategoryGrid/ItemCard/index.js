3bc8f22ba9f990f729351b322ec623c591af94df
alec aivazis <alec@aivazis.com>
2017-06-04 01:06:43 -0700

    category editor no longer covers scroll side view

--- App/CategoryGrid/ItemCard/index.js
+++ App/CategoryGrid/ItemCard/index.js
@@ -13,75 +13,14 @@ const animationDuration = 100
 
 class ItemCard extends PureComponent {
     state = {
-        showEditor: false,
         layout: {},
-        origin: {},
-    }
-
-    _openEditor() {
-        this.setState({
-            showEditor: true,
-        }, () => {
-            // we have a few things we need to animate at once
-            Animated.parallel([
-                Animated.timing(this.state.layout.width, {
-                    toValue: 778,
-                    duration: animationDuration,
-                }),
-                Animated.timing(this.state.layout.height, {
-                    toValue: 684,
-                    duration: animationDuration,
-                }),
-                Animated.timing(this.state.layout.x, {
-                    toValue: 248,
-                    duration: animationDuration,
-                }),
-                Animated.timing(this.state.layout.y, {
-                    toValue: 85,
-                    duration: animationDuration,
-                })
-            ]).start()
-        })
-    }
-
-    _closeEditor() {
-        // we have a few things we need to animate at once
-        Animated.parallel([
-            Animated.timing(this.state.layout.width, {
-                toValue: this.state.origin.width,
-                duration: animationDuration,
-            }),
-            Animated.timing(this.state.layout.height, {
-                toValue: this.state.origin.height,
-                duration: animationDuration,
-            }),
-            Animated.timing(this.state.layout.x, {
-                toValue: this.state.origin.x,
-                duration: animationDuration,
-            }),
-            Animated.timing(this.state.layout.y, {
-                toValue: this.state.origin.y,
-                duration: animationDuration,
-            })
-        ]).start(() => {
-            this.setState({
-                showEditor: false,
-            })
-        })
-
     }
 
     componentDidMount() {
         setTimeout(() => {
             this._root.measure((_, __, width, height, x, y) => {
                 this.setState({
-                    origin: { x, y, width, height },
-                    layout: {
-                        x: new Animated.Value(x),
-                        y: new Animated.Value(y),
-                        width: new Animated.Value(width),
-                        height: new Animated.Value(height),
-                    }
+                    layout: { x, y, width, height },
                 })
             })
         }, 0)
@@ -89,46 +28,20 @@ class ItemCard extends PureComponent {
 
     render() {
         // grab used components
-        const {style, children, item, ...unused} = this.props
+        const {style, children, item, openEditor, ...unused} = this.props
 
         return (
             <Card style={[styles.container, style]} {...unused}>
                 <View style={{flex: 1}} ref={ele => this._root = ele}>
-                    <ItemSummary item={item} onPress={this._openEditor} />
+                    <ItemSummary item={item}/>
                 </View>
-                {this.state.showEditor && (
-                    <Modal transparent>
-                        <Animated.ScrollView
-                            style={{
-                                width: this.state.layout.width,
-                                height: this.state.layout.height,
-                                top: this.state.layout.y,
-                                left: this.state.layout.x,
-                                position: 'absolute',
-                                backgroundColor: 'red',
-                            }}
-                        >
-                            <Button onPress={this._closeEditor}>
-                                close
-                            </Button>
-                        </Animated.ScrollView>
-                    </Modal>
-                )}
                 <View style={styles.actions}>
-                    <CardAction>
+                    <CardAction onPress={() => openEditor(this.state.layout)} >
                         special
                     </CardAction>
                 </View>
             </Card>
         )
-    }
-
-    constructor(...args) {
-        super(...args)
-
-        // function binds
-        this._openEditor = this._openEditor.bind(this)
-        this._closeEditor = this._closeEditor.bind(this)
     }
 }
 
--- App/CategoryGrid/index.js
+++ App/CategoryGrid/index.js
@@ -1,7 +1,8 @@
 // external imports
 import React, { PureComponent } from 'react'
-import { View, Text, FlatList, ScrollView } from 'react-native'
+import { View, Text, FlatList, ScrollView, Modal, Animated } from 'react-native'
 import { TabBar, TabView, Tab } from '../../quark/native'
+import { Button } from '../../quark'
 import { Route, Link } from 'react-router-native'
 import { gql, graphql } from 'react-apollo'
 import { TabViewAnimated } from 'react-native-tab-view'
@@ -10,38 +11,170 @@ import _ from 'lodash'
 import ItemCard from './ItemCard'
 import styles from './styles'
 
-const CategoryGrid = ({style, data, ...rest}) => {
-    // if we are still loading the list
-    if (data.loading) {
-        // return the loading indicator
-        return <Text>loading...</Text>
+const animationDuration = 100
+
+class CategoryGrid extends React.Component {
+    state = {
+        showEditor: false,
+        modal: {},
+        origin: {},
+        viewport: {},
     }
 
-    // chunk the data into rows once
-    const categories = data.allCategories.map(cat => ({key: cat.name, rows: _.chunk(cat.items, 3)}))
+    _openEditor(origin) {
+        // get the used params
+        const { viewport } = this.state
 
-    return (
-        <TabView data={categories} numTabs={1}>
-            {/* the page for each category */}
-            {({rows, key}) => (
-                <ScrollView style={styles.gridContainer} key={key}>
-                    {rows.map((row, rowIdx) => (
-                        <View style={styles.row} key={rowIdx}>
-                            {[
-                                ...row.map(item => <ItemCard item={item} key={item.id} style={styles.card}/>),
-                                ...Array.apply(null, {length: 3 - row.length}).map(Number.call, Number).map(
-                                    (_, i) => (
-                                        <View style={[styles.placeholder, styles.card]} key={`${rowIdx}:${i}`}/>
-                                    )
-                                )
-                            ]}
-                        </View>
-                    ))}
-                </ScrollView>
-            )}
-        </TabView>
-    )
+        // shift the origin to accomodate the viewport
+        const relOrigin = {
+            x: origin.x - 247,
+            y: origin.y - 64,
+            height: origin.height,
+            width: origin.width,
+        }
+
+        this.setState({
+            showEditor: true,
+            origin: relOrigin,
+            modal: {
+                x: new Animated.Value(relOrigin.x),
+                y: new Animated.Value(relOrigin.y),
+                width: new Animated.Value(relOrigin.width),
+                height: new Animated.Value(relOrigin.height),
+            }
+        }, () => {
+            // we have a few things we need to animate at once
+            Animated.parallel([
+                Animated.timing(this.state.modal.width, {
+                    toValue: this.state.viewport.width,
+                    duration: animationDuration,
+                }),
+                Animated.timing(this.state.modal.height, {
+                    toValue: this.state.viewport.height,
+                    duration: animationDuration,
+                }),
+                Animated.timing(this.state.modal.x, {
+                    toValue: 0,
+                    duration: animationDuration,
+                }),
+                Animated.timing(this.state.modal.y, {
+                    toValue: 0,
+                    duration: animationDuration,
+                })
+            ]).start()
+        })
+    }
+
+    _closeEditor() {
+        // we have a few things we need to animate at once
+        Animated.parallel([
+            Animated.timing(this.state.modal.width, {
+                toValue: this.state.origin.width,
+                duration: animationDuration,
+            }),
+            Animated.timing(this.state.modal.height, {
+                toValue: this.state.origin.height,
+                duration: animationDuration,
+            }),
+            Animated.timing(this.state.modal.x, {
+                toValue: this.state.origin.x,
+                duration: animationDuration,
+            }),
+            Animated.timing(this.state.modal.y, {
+                toValue: this.state.origin.y,
+                duration: animationDuration,
+            })
+        ]).start(() => {
+            this.setState({
+                showEditor: false,
+            })
+        })
+    }
+
+    _updateViewport() {
+        // figure out how big the viewport is
+        this._root.measure((_, __, width, height, x, y) => {
+            // save the viewport dimensions
+            this.setState({
+                viewport: {
+                    width,
+                    height,
+                    x,
+                    y,
+                }
+            })
+        })
+    }
+
+    componentDidMount() {
+        // update the notion of the viewport
+        setTimeout(this._updateViewport, 0)
+    }
+
+    constructor(...args) {
+        super(...args)
+
+        // function binds
+        this._openEditor = this._openEditor.bind(this)
+        this._closeEditor = this._closeEditor.bind(this)
+        this._updateViewport = this._updateViewport.bind(this)
+    }
+
+    render() {
+        const { style, data, ...rest } = this.props
+
+        return (
+            <View style={{flex: 1}} ref={ele => this._root = ele}>
+                { data.loading ? (
+                    <Text>loading...</Text>
+                ) : (
+                    <TabView
+                        data={data.allCategories.map(cat => ({key: cat.name, rows: _.chunk(cat.items, 3)}))}
+                        numTabs={6}
+                        style={styles.container}
+                    >
+                        {/* the page for each category */}
+                        {({rows, key}) => (
+                            <ScrollView style={styles.gridContainer} key={key}>
+                                {rows.map((row, rowIdx) => (
+                                    <View style={styles.row} key={rowIdx}>
+                                        {[
+                                            ...row.map(item => (
+                                                <ItemCard item={item} key={item.id} style={styles.card} openEditor={this._openEditor}/>
+                                            )),
+                                            ...Array.apply(null, {length: 3 - row.length}).map(Number.call, Number).map(
+                                                (_, i) => (
+                                                    <View style={[styles.placeholder, styles.card]} key={`${rowIdx}:${i}`}/>
+                                                )
+                                            )
+                                        ]}
+                                    </View>
+                                ))}
+                            </ScrollView>
+                        )}
+                    </TabView>
+                )}
+                {this.state.showEditor && (
+                    <Animated.ScrollView
+                        style={{
+                            width: this.state.modal.width,
+                            height: this.state.modal.height,
+                            top: this.state.modal.y,
+                            left: this.state.modal.x,
+                            position: 'absolute',
+                            backgroundColor: 'red',
+                        }}
+                    >
+                        <Button onPress={this._closeEditor}>
+                            close
+                        </Button>
+                    </Animated.ScrollView>
+                )}
+            </View>
+        )
+    }
 }
+
 
 // the component data requirements
 const query = gql`
